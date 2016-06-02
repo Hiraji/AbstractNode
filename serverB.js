6bb95b3c46495236d3893f58ab933d1a2f303c28
Hiraji <hiraji@compumatrice.com>
2016-06-02 15:55:11 +0530

    br

--- serverB.js
+++ serverB.js
@@ -25,13 +25,13 @@ var connection  = require('express-myconnection'),
     mysql = require('mysql');
 
 app.use(
- 
+
     connection(mysql,{
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        port     : '3306',
-        database : 'ced',
+        host     : 'ec2-50-19-219-148.compute-1.amazonaws.com',
+        user     : 'dowggepadcqdej',
+        password : 'djS9n9yKoM1waUYRusvBbUF-Ay',
+        port     : '5432',
+        database : 'de9j2qm2r0rvlu',
         multipleStatements: true,
         debug    : false //set true if you wanna see debug logger
     },'request')
@@ -40,8 +40,8 @@ app.use(
 app.all('/*', function(req, res, next) {
       var md = new MobileDetect(req.headers['user-agent']);
 console.log( md.os() );
-console.log( md.userAgent() ); 
-  
+console.log( md.userAgent() );
+
   // Set custom headers for CORS
   res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
   if (req.method == 'OPTIONS') {
@@ -67,6 +67,6 @@ app.use('/',require('./routes/indexR'));
 
 //start Server
 var server = app.listen(process.env.PORT || 7000,function(){
- 
+
    console.log("Listening to port %s",server.address().port);
 });
